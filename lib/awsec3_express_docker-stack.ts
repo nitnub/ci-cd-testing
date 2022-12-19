792d774e4b2671d00926c8370cf4a4929abf5ab0
Nick Laptop <45341924+nitnub@users.noreply.github.com>
2022-12-19 04:47:07 -0500

    tsc file imports

--- lib/awsec3_express_docker-stack.ts
+++ lib/awsec3_express_docker-stack.ts
@@ -1,37 +1,36 @@
-import cdk from "@aws-cdk/core";
-import ec2 from "@aws-cdk/aws-ec2";
-import ecs from "@aws-cdk/aws-ecs";
-import elbv2 from "@aws-cdk/aws-elasticloadbalancingv2";
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
 
 /**
  * Enter DockerHub path here (example -> nitnub/ci-cd-testing)
  */
 
-const dockerHubPath = 'nitnub/ci-cd-testing'
-
+const dockerHubPath = 'nitnub/ci-cd-testing';
 
 export class Awsec3ExpressDockerStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Virtual Private Cloud
-    const vpc = new ec2.Vpc(this, "helloVpc", { maxAzs: 2 });
+    const vpc = new ec2.Vpc(this, 'helloVpc', { maxAzs: 2 });
 
     // Create an ECS cluster
-    var cluster = new ecs.Cluster(this, "Cluster", { vpc });
-    cluster.addCapacity("DefaultAutoScalingGroup", {
-      instanceType: new ec2.InstanceType("t2.micro"),
+    var cluster = new ecs.Cluster(this, 'Cluster', { vpc });
+    cluster.addCapacity('DefaultAutoScalingGroup', {
+      instanceType: new ec2.InstanceType('t2.micro'),
       maxCapacity: 3,
     });
 
     // hello service
     const helloTaskDefinition = new ecs.Ec2TaskDefinition(
       this,
-      "hello-task-definition",
+      'hello-task-definition',
       {}
     );
     // Spinning up containers and pulling image from docker hub
-    const helloContainer = helloTaskDefinition.addContainer("hello", {
+    const helloContainer = helloTaskDefinition.addContainer('hello', {
       image: ecs.ContainerImage.fromRegistry(dockerHubPath),
       memoryLimitMiB: 128,
     });
@@ -40,31 +39,31 @@ export class Awsec3ExpressDockerStack extends cdk.Stack {
       containerPort: 3000,
     });
 
-    const helloService = new ecs.Ec2Service(this, "hello-service", {
+    const helloService = new ecs.Ec2Service(this, 'hello-service', {
       cluster: cluster,
       desiredCount: 3,
       taskDefinition: helloTaskDefinition,
     });
     // Internet facing load balancer for the frontend services
 
-    const externalLB = new elbv2.ApplicationLoadBalancer(this, "external", {
+    const externalLB = new elbv2.ApplicationLoadBalancer(this, 'external', {
       vpc: vpc,
       internetFacing: true,
     });
 
-    const externalListener = externalLB.addListener("PublicListener", {
+    const externalListener = externalLB.addListener('PublicListener', {
       port: 80,
       open: true,
     });
 
     // run helloService defined above and expose on port 80
-    externalListener.addTargets("greeter", {
+    externalListener.addTargets('greeter', {
       port: 80,
       targets: [helloService],
     });
 
     // external URL for exposed port
-    new cdk.CfnOutput(this, "ExternalDNS", {
+    new cdk.CfnOutput(this, 'ExternalDNS', {
       value: externalLB.loadBalancerDnsName,
     });
   }
