f9ccf74df5f72f8c0abe8512ccc5ffbdd997f743
Nick Laptop <45341924+nitnub@users.noreply.github.com>
2022-12-19 17:42:13 -0500

    update script pathhs

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,9 @@
-echo 'run after_install.sh: ' >> /home/ec2-user/ci-cd-instance-v2/deploy.log
+echo 'run after_install.sh: ' >> /home/ec2-user/ci-cd-testing/deploy.log
 
-echo 'cd /home/ec2-user/ci-cd-instance-v2' >> /home/ec2-user/ci-cd-instance-v2/deploy.log
-cd /home/ec2-user/ci-cd-instance-v2 >> /home/ec2-user/ci-cd-instance-v2/deploy.log
+echo 'cd /home/ec2-user/ci-cd-testing' >> /home/ec2-user/ci-cd-testing/deploy.log
+cd /home/ec2-user/ci-cd-testing >> /home/ec2-user/ci-cd-testing/deploy.log
 
-echo 'npm install' >> /home/ec2-user/ci-cd-instance-v2/deploy.log 
-npm install >> /home/ec2-user/ci-cd-instance-v2/deploy.log
+echo 'npm install' >> /home/ec2-user/ci-cd-testing/deploy.log 
+npm install >> /home/ec2-user/ci-cd-testing/deploy.log
 
 
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/ci-cd-instance-v2/deploy.log
+echo 'run application_start.sh: ' >> /home/ec2-user/ci-cd-testing/deploy.log
 
-echo 'pm2 restart ci-cd-testing' >> /home/ec2-user/ci-cd-instance-v2/deploy.log
-pm2 restart nodejs-express-app >> /home/ec2-user/ci-cd-instance-v2/deploy.log
+echo 'pm2 restart ci-cd-testing' >> /home/ec2-user/ci-cd-testing/deploy.log
+pm2 restart nodejs-express-app >> /home/ec2-user/ci-cd-testing/deploy.log
