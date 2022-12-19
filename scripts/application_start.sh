81f98596f1eaf059007d87ff04ae385740273c81
Nick Laptop <45341924+nitnub@users.noreply.github.com>
2022-12-19 12:05:43 -0500

    update restart script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,7 +2,7 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/ci-cd-instance-v2/deploy.log
 
-echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/ci-cd-instance-v2/deploy.log
+echo 'pm2 restart server' >> /home/ec2-user/ci-cd-instance-v2/deploy.log
 pm2 restart nodejs-express-app >> /home/ec2-user/ci-cd-instance-v2/deploy.log
 
 
