00e459ffe0774c4efd5c4344c37804cfb4fab0ec
Nick Laptop <45341924+nitnub@users.noreply.github.com>
2022-12-20 06:07:12 -0500

    text file test

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,3 +4,4 @@ echo 'run application_start.sh: ' >> /home/ec2-user/ci-cd-testing/deploy.log
 
 echo 'pm2 restart ci-cd-testing' >> /home/ec2-user/ci-cd-testing/deploy.log
 pm2 restart nodejs-express-app >> /home/ec2-user/ci-cd-testing/deploy.log
+echo 'this is a test, sent at 6:06AM 12/20/22' >> testfile.txt
